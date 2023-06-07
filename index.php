b6f42c5a5ad79c257dc1da68a599b67b11660871
José Domingo Muñoz <josedom24@gmail.com>
2023-06-07 20:29:14 +0200

    cambio

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
                 <div class="col-lg-12">
                     <h1>Curso OpenShift v4</h1>
                     <h2>Aplicación escrita en PHP!!!</h2>
+			<h3>Aplicación desplegada con Webhook</h3>
                     <p>Fecha: <strong>
                         <?php
                         $today = date("d/m/Y");
